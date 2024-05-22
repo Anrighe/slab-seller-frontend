d288424d18f6e03d2a6562cd4b0227cc8ae52119
Anrighe <assoonpc@gmail.com>
2024-05-22 22:32:45 +0200

    minor modifications + new openapi

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { StoreComponent } from "./store.component";
-import { StoreRoutingModule } from "./store.routing";
+import { StoreRoutingModule } from "./store.routing-module";
 import { CommonModule } from "@angular/common";
 
 
@@ -11,9 +11,6 @@ import { CommonModule } from "@angular/common";
         CommonModule,
         RouterModule,
         StoreRoutingModule,
-        //RouterModule.forChild([
-        //    { path: '', component: StoreComponent },
-        //]),
     ]
 })
 export class StoreModule {
